5dac7e2816fe77b4a3d3bae987ec5e5c4e142cb6
kiki7000 <51841836+kiki7000@users.noreply.github.com>
2021-03-29 05:18:16 +0000

    Deploying to gh-pages from @ Team-Kat/discommu-frontend@605ae62039e9fe0ca3e3bf4bc4ee0ba7aa739ac2 🚀

--- dist/pages/NewCategory.js
+++ dist/pages/NewCategory.js
@@ -1,6 +1,6 @@
 import React, {Component} from "../../_snowpack/pkg/react.js";
 import {req} from "../utils/request.js";
-import {errorAlert} from "../utils/alert.js";
+import {errorAlert, confirmAlert} from "../utils/alert.js";
 class NewCategory extends Component {
   constructor(props) {
     super(props);
@@ -13,7 +13,31 @@ class NewCategory extends Component {
     if (!localStorage.token)
       await errorAlert({text: "\uB85C\uADF8\uC778\uC774 \uD544\uC694\uD55C \uD654\uBA74\uC785\uB2C8\uB2E4"});
   }
-  createCategory = async () => {
+  createCategory = async (e) => {
+    e.preventDefault();
+    const alertRes = await confirmAlert({
+      title: "\uC815\uB9D0 \uC0DD\uC131\uC744 \uD560\uAE4C\uC694?",
+      confirmButtonText: "\uC0DD\uC131"
+    });
+    if (!alertRes.isConfirmed)
+      return;
+    const res = await req({
+      query: `
+                mutation {
+                    addCategory(data: {
+                        name: "${this.state.name}"
+                        description: "${this.state.description}"
+                    })
+                }
+            `
+    });
+    if (res.data && res.data.addCategory) {
+      this.props.history.push(`/category/${this.state.name}`);
+    } else {
+      await errorAlert({
+        title: "\uC0DD\uC131\uC744 \uC2E4\uD328\uD588\uC2B5\uB2C8\uB2E4"
+      });
+    }
   };
   render() {
     return /* @__PURE__ */ React.createElement("div", {
@@ -25,10 +49,16 @@ class NewCategory extends Component {
       className: "mt-4"
     }, /* @__PURE__ */ React.createElement("input", {
       className: "w-full h-10 appearance-none rounded-xl border border-gray-400 block pl-2 py-2 bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none",
-      placeholder: "\uC774\uB984"
+      placeholder: "\uC774\uB984",
+      onChange: (e) => {
+        this.setState({name: e.target.value});
+      }
     }), /* @__PURE__ */ React.createElement("input", {
       className: "w-full h-10 mt-2 appearance-none rounded-xl border border-gray-400 block pl-2 py-2 bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none",
-      placeholder: "\uC124\uBA85"
+      placeholder: "\uC124\uBA85",
+      onChange: (e) => {
+        this.setState({description: e.target.value});
+      }
     }), /* @__PURE__ */ React.createElement("button", {
       type: "submit",
       className: "w-full h-10 text-sm bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-xl border-r-2 mt-2"
