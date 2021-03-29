605ae62039e9fe0ca3e3bf4bc4ee0ba7aa739ac2
kiki7000 <devkiki7000@gmail.com>
2021-03-29 14:17:27 +0900

    feat: Add Category

--- src/pages/NewCategory.jsx
+++ src/pages/NewCategory.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import { req } from '../utils/request';
-import { errorAlert } from '../utils/alert';
+import { errorAlert, confirmAlert } from '../utils/alert';
 
 class NewCategory extends Component {
     constructor (props) {
@@ -17,8 +17,32 @@ class NewCategory extends Component {
             await errorAlert({ text: '로그인이 필요한 화면입니다' })
     }
 
-    createCategory = async () => {
+    createCategory = async e => {
+        e.preventDefault()
+        const alertRes = await confirmAlert({
+            title: '정말 생성을 할까요?',
+            confirmButtonText: '생성'
+        })
+        if (!alertRes.isConfirmed) return;
+        const res = await req({
+            query: `
+                mutation {
+                    addCategory(data: {
+                        name: "${this.state.name}"
+                        description: "${this.state.description}"
+                    })
+                }
+            `
+        })
 
+        if (res.data && res.data.addCategory) {
+            this.props.history.push(`/category/${this.state.name}`)
+        }
+        else {
+            await errorAlert({
+                title: '생성을 실패했습니다'
+            })
+        }
     }
 
     render() {
@@ -26,8 +50,12 @@ class NewCategory extends Component {
             <div className = 'w-4/5 mx-auto mt-20'>
                 <h2 className = 'text-2xl font-semibold text-gray-900 select-none'>카테고리 생성</h2>
                 <form onSubmit = {this.createCategory} className = 'mt-4'>
-                    <input className = 'w-full h-10 appearance-none rounded-xl border border-gray-400 block pl-2 py-2 bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none' placeholder = '이름' />
-                    <input className = 'w-full h-10 mt-2 appearance-none rounded-xl border border-gray-400 block pl-2 py-2 bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none' placeholder = '설명' />
+                    <input className = 'w-full h-10 appearance-none rounded-xl border border-gray-400 block pl-2 py-2 bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none' placeholder = '이름' onChange = {e => {
+                        this.setState({ name: e.target.value })
+                    }} />
+                    <input className = 'w-full h-10 mt-2 appearance-none rounded-xl border border-gray-400 block pl-2 py-2 bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none' placeholder = '설명' onChange = {e => {
+                        this.setState({ description: e.target.value })
+                    }} />
                     <button type = 'submit' className = 'w-full h-10 text-sm bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-xl border-r-2 mt-2'>
                         만들기!
                     </button>
